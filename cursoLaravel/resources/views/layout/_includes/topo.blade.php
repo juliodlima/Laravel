caaccef8001df5404d4c16e72638c2345c52c87c
juliodlima <juliodlima@yahoo.com.br>
2019-07-29 12:08:02 -0300

    sistema de login e logout

--- cursoLaravel/resources/views/layout/_includes/topo.blade.php
+++ cursoLaravel/resources/views/layout/_includes/topo.blade.php
@@ -19,18 +19,26 @@
                     <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="/">Home</a></li>
-                        <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
-                        {{-- <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li> --}}
+                        @if (Auth::guest())
+                            <li><a href="{{route('site.login')}}">Login</a></li>
+                        @else
+                            <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+                            <li><a href="#">{{Auth::user()->name}}</a></li>
+                            <li><a href="{{route('site.login.sair')}}">Sair</a></li>
+                        @endif
                     </ul>
                 </div>
             </nav>
         
             <ul class="sidenav" id="mobile">
                 <li><a href="/">Home</a></li>
-                <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
-                {{-- <li><a href="collapsible.html">Javascript</a></li>
-                <li><a href="mobile.html">Mobile</a></li> --}}
+                @if (Auth::guest())
+                    <li><a href="{{route('site.login')}}">Login</a></li>
+                @else
+                    <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+                    <li><a href="#">{{Auth::user()->name}}</a></li>
+                    <li><a href="{{route('site.login.sair')}}">Sair</a></li>
+                @endif
             </ul>
         </header>
             
