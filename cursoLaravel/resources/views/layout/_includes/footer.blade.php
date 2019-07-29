a89a3b5dc35bd1dcdce4d384b0b5485923fc9355
juliodlima <juliodlima@yahoo.com.br>
2019-07-28 23:42:17 -0300

    layout materialize e lista registros

--- cursoLaravel/resources/views/layout/_includes/footer.blade.php
+++ cursoLaravel/resources/views/layout/_includes/footer.blade.php
@@ -1,3 +1,12 @@
-</body>
 
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        Materialize.updateTexFields();
+        $('.sidenav').sidenav();
+    });
+</script>
+</body>
 </html>
